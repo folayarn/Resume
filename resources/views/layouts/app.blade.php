386880207f1df2e84ec20f4b8eca6b403cc15e61
Folayan Adesola <48876487+folayarn@users.noreply.github.com>
2020-10-04 15:01:14 +0100

    Update app.blade.php

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <link rel="icon" href="{{asset('OL.png')}}" type="image/icon type">
+        <link rel="icon" href="{{secure_asset('OL.png')}}" type="image/icon type">
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
     <link rel="stylesheet" href="{{asset('css/app.css')}}">
-    <script src="{{asset('js/app.js')}}"></script>
-    <script src="{{asset('js/boostrap.js')}}"></script>
+    <script src="{{secure_asset('js/app.js')}}"></script>
+    <script src="{{secure_asset('js/boostrap.js')}}"></script>
     <title>{{config('app.name','MyCV')}}</title>
 
 
